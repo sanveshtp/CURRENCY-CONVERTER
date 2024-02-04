bb80bc973facd0a4296c4fbe2842478c5d6b2ab8
sanveshtp <sanveshthumkunta@gmail.com>
2024-02-04 23:42:20 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+let fromcurr = document.getElementById("fromcurr")
+let tocurr = document.getElementById("tocurr")
+let k = document.querySelectorAll("select")
+let fromflag = document.getElementById("fromflag")
+let toflag = document.getElementById("toflag")
+let rev = document.getElementById("rev");
+
+// updating options
+for (let i in countryList) {
+    if (i == "USD") {
+        fromcurr.innerHTML += `<option value=${i} selected >${i}</option>`
+    } else if (i == "INR") {
+        tocurr.innerHTML += `<option value=${i} selected >${i}</option>`
+    } else {
+        fromcurr.innerHTML += `<option value=${i}>${i}</option>`
+        tocurr.innerHTML += `<option value=${i}>${i}</option>`
+    }
+
+}
+
+rev.addEventListener("click", () => {
+    let temp = fromcurr.value
+    console.log(temp);
+    fromcurr.value = tocurr.value
+    tocurr.value = temp
+    updateflag(fromflag, fromcurr.value);
+    updateflag(toflag, tocurr.value);
+})
+
+
+// updating flags
+const updateflag = (a, b) => {
+    a.setAttribute("src", `https://flagsapi.com/${countryList[b]}/flat/64.png`);
+}
+k[0].addEventListener("change", (e) => {
+    updateflag(fromflag, fromcurr.value);
+})
+k[1].addEventListener("change", (e) => {
+    updateflag(toflag, tocurr.value);
+})
+
+
+// exchange button
+let exchbut = document.getElementById("exchbut")
+exchbut.addEventListener("click", (e) => {
+    e.preventDefault()
+    conve()
+
+})
+
+
+async function conve() {
+    // fetching the api
+    let resp = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${(fromcurr.value).toLowerCase()}/${(tocurr.value).toLowerCase()}.json`);
+    data = await resp.json();
+
+    // targetting the amount
+    let amount = document.getElementById("convnu").value
+    let resamt = Math.floor(amount * (data[(tocurr.value).toLowerCase()]))
+    let resdisp = document.getElementById("res")
+    resdisp.innerHTML = `${amount} ${fromcurr.value} = ${resamt} ${tocurr.value}`
+}
+
+
